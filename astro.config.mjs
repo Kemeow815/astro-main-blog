82237ef798f160c24d93d3e81433a7fb357c8d5f
Kemeow815 <kemiao@kmblog.icu>
2025-04-25 12:27:11 +0800

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://kemeow.top',
+  site: 'https://kmblog.kemeow.top',
   // base: '/docs',
   trailingSlash: 'never',
 
